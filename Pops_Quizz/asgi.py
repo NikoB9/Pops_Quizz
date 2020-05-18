13022556689e2d6f095edc21430bfe99dcff97ca
Niko <Nik@Niko.Niko>
2020-05-18 11:28:05 +0200

    Premier jet de l'application
    Installation Initiale de Django

---
+++ Pops_Quizz/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Pops_Quizz project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Pops_Quizz.settings')
+
+application = get_asgi_application()
