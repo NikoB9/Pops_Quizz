eff905f92d56400609ec96eeeed998f170114838
TonyLim <tony-lim@hotmail.fr>
2020-05-22 17:18:55 +0200

    wip

--- Quizz/views.py
+++ Quizz/views.py
@@ -7,128 +7,142 @@ from django.http import HttpResponseRedirect
 
 # Create your views here.
 
-#ACCES MODEL
+# ACCES MODEL
 from .forms import *
 
 # Import requests
 from Quizz.requests.request_user import *
 from Quizz.requests.request_form import *
 from Quizz.requests.request_question import *
+from Quizz.requests.request_game import *
 
-#regex
 
-#FOR JSON RESPONSE
+# regex
 
-#OS lib
-#settings
-#date
+# FOR JSON RESPONSE
+
+# OS lib
+# settings
+# date
 
 def index(request):
-    """return HttpResponse("<h1 style="text-align:center">Page principal</h1>")"""
-    """Liste pour créer le menu"""
     allforms = getAllForms()
-    return render(request, "home/index.html", {'allforms' : allforms})
+    return render(request, "home/index.html", {'allforms': allforms})
+
 
 def openform(request, idform):
+    game_name = request.POST.get('game_name', None)
+    slot_max = request.POST.get('slot_max', None)
+    is_public = request.POST.get('is_public', None)
+    login = request.session['login']
 
-	f = getFormsById(idform)
-	questions = getQuestionsByForm(f)
-	f.questions = getPossibleAnswersByQuestions(questions)
-	print(f)
+    game = create_game(idform, login, game_name, is_public, slot_max)
 
-	return render(request, "home/forms.html", {'form' : f})
+    user = getUserByLogin(login)
+
+    player = Player()
+    player.user = user
+    player.game = game
+    player.score = 0
+    player.save()
+
+    f = getFormsById(idform)
+    questions = getQuestionsByForm(f)
+    f.questions = getPossibleAnswersByQuestions(questions)
+
+    return render(request, "home/forms.html", {'form': f})
+
 
 def users(request):
-    """return HttpResponse("<h1 style="text-align:center">Page principal</h1>")"""
-    """Liste pour créer le menu"""
     users = getAllUsers()
 
     return render(request, "home/users.html", {'users': users})
 
-def createUser(request):
-    
-	login = request.POST.get('loginco', None)
-	email = request.POST.get('emailco', None)
-	password = request.POST.get('passwordco', None)
-	password_validation = request.POST.get('passwordco2', None)
 
-	if loginExist(login) :
+def create_user(request):
+    login = request.POST.get('loginco', None)
+    email = request.POST.get('emailco', None)
+    password = request.POST.get('passwordco', None)
+    password_validation = request.POST.get('passwordco2', None)
 
-		data = {
-    	'is_valid':  False,
-    	'error_message': "Le pseudo existe déjà."   
-    	}
+    if loginExist(login):
 
-	elif emailExist(email) :
+        data = {
+            'is_valid': False,
+            'error_message': "Le pseudo existe déjà."
+        }
 
-		data = {
-    	'is_valid':  False,
-    	'error_message': "L'email existe déjà."   
-    	}
+    elif emailExist(email):
 
-	elif password != password_validation :
+        data = {
+            'is_valid': False,
+            'error_message': "L'email existe déjà."
+        }
 
-		data = {
-    	'is_valid':  False,
-    	'error_message': "Le mot de passe de confirmation est différent."   
-    	}
+    elif password != password_validation:
 
-	else :
-        
-		createUserBD(login, email, password)
-        
-		data = {
-    	'is_valid':  True,
-    	}
+        data = {
+            'is_valid': False,
+            'error_message': "Le mot de passe de confirmation est différent."
+        }
 
-	return JsonResponse(data)
+    else:
+
+        createUserBD(login, email, password)
+
+        data = {
+            'is_valid': True,
+        }
+
+    return JsonResponse(data)
+
 
 def connectUser(request):
-    
-	login = request.POST.get('login', None)
-	password = request.POST.get('password', None)
+    login = request.POST.get('login', None)
+    password = request.POST.get('password', None)
 
-	if loginExist(login) :
+    if loginExist(login):
 
-		if valideUser(login, password):
-    		
-			data = {
-		    	'is_valid':  True,
-		    	}
+        if valideUser(login, password):
 
-			request.session['login'] = login
+            data = {
+                'is_valid': True,
+            }
 
-		else :
+            request.session['login'] = login
 
-			data = {
-	    	'is_valid':  False,
-	    	'error_message': "Le mot de passe est incorrect."   
-	    	}
+        else:
 
-	else :
+            data = {
+                'is_valid': False,
+                'error_message': "Le mot de passe est incorrect."
+            }
 
-		data = {
-    	'is_valid':  False,
-    	'error_message': "Le pseudo n'existe pas."   
-    	}
+    else:
 
+        data = {
+            'is_valid': False,
+            'error_message': "Le pseudo n'existe pas."
+        }
 
-	return JsonResponse(data)
+    return JsonResponse(data)
 
+def create_game(request, id_form):
 
+    f = getFormsById(id_form)
+    questions = getQuestionsByForm(f)
+    f.questions = getPossibleAnswersByQuestions(questions)
+    print(f)
+
+    return render(request, "home/create-game.html", {'form': f})
 
 def creation(request):
-    
     return render(request, "home/creation.html")
 
+
 def categories(request):
-    
     return render(request, "home/categories.html")
 
+
 def resultats(request):
-    
     return render(request, "home/resultats.html")
-
-
-
-
