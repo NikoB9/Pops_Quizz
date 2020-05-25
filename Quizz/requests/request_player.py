67031ea8927ec73cc208496bb50c4d4b944572f1
TonyLim <tony-lim@hotmail.fr>
2020-05-25 21:05:50 +0200

    finalisation du scoring, fix input error

--- Quizz/requests/request_player.py
+++ Quizz/requests/request_player.py
@@ -30,7 +30,6 @@ def calculate_score(player):
         elif q['question'].answer_type.type == "QCM":
             for possible_answer in q['answers']:
                 uas = user_selected_possible_answer(possible_answer, player)
-                print(uas[0])
                 if len(uas) > 0 and uas[0].value == "1":
                     if possible_answer.correct:
                         score += 1
@@ -42,7 +41,7 @@ def calculate_score(player):
             for possible_answer in q['answers']:
                 possible_input_values.append(possible_answer.value.strip())
             user_answer_input = get_input_response_by_question_by_player(q['question'], player)
-            if user_answer_input.value in possible_input_values:
+            if user_answer_input.value.strip() in possible_input_values:
                 score += 1
     player.score = score
     player.save()
