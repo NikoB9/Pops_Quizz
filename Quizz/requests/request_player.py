a740ca5b035c58314e0decfa33c2d6e9b648d966
TonyLim <tony-lim@hotmail.fr>
2020-05-25 16:01:43 +0200

    algo de scoring done

--- Quizz/requests/request_player.py
+++ Quizz/requests/request_player.py
@@ -5,34 +5,33 @@ from Quizz.requests.request_user_answers import *
 from Quizz.requests.request_possible_answer import *
 
 
-def get_player_by_login(login):
-    return Player.objects.get(login=login)
+def get_player_by_id(id):
+    return Player.objects.get(id=id)
 
 
 def get_players_by_game_order_by_score_desc(game):
     return Player.objects.filter(game=game).order_by('-score')
 
 
-def calculate_score(login):
-    player = get_player_by_login(login)
-    questions = getQuestionsByForm(player.game.form)
-    pa = getPossibleAnswersByQuestions(questions)
+def calculate_score(player_id):
+    player = get_player_by_id(player_id)
+    questions = getPossibleAnswersByQuestions(getQuestionsByForm(player.game.form))
     score = 0
-
     for q in questions:
-        if q.question.answer_type.type == "QCM" or q.question.answer_type.type == "UNIQUE_ANSWER":
-            for possible_answer in q.answers:
-                if(user_selected_possible_answer(possible_answer)):
+        if q['question'].answer_type.type == "QCM" or q['question'].answer_type.type == "UNIQUE_ANSWER":
+            for possible_answer in q["answers"]:
+                if user_selected_possible_answer(possible_answer, player):
                     if possible_answer.correct:
                         score += 1
                     else:
                         # decrease score by one if QCM
-                        score -= 1 if q.question.answer_type.type == "QCM" else 0
-        elif q.question.answer_type.type == "INPUT":
+                        score -= 0.5 if q['question'].answer_type.type == "QCM" else 0
+        elif q['question'].answer_type.type == "INPUT":
             possible_input_values = []
-            for possible_answer in q.answers:
+            for possible_answer in q["answers"]:
                 possible_input_values.append(possible_answer.value.strip())
-            if get_input_response_of_a_question(q.question) in possible_input_values:
+            user_answer_input = get_input_response_by_question_by_player(q['question'], player)
+            if user_answer_input.value in possible_input_values:
                 score += 1
     player.score = score
     player.save()
--- Quizz/requests/request_user_answers.py
+++ Quizz/requests/request_user_answers.py
@@ -8,10 +8,10 @@ def get_user_answers_of_a_player(player):
     return UserAnswers.objects.filter(player=player)
 
 
-def user_selected_possible_answer(possible_answer):
-    return len(UserAnswers.objects.filter(possible_answer=possible_answer)) > 0
+def user_selected_possible_answer(possible_answer, player):
+    return len(UserAnswers.objects.filter(possible_answer=possible_answer, player=player)) > 0
 
 
-def get_input_response_of_a_question(question):
-    possible_answers = get_possible_answer_of_a_question(question)
-    return UserAnswers.objects.get(possible_answer=possible_answers[0])
+def get_input_response_by_question_by_player(question, player):
+    possible_answer = get_possible_answer_of_a_question(question)[0]
+    return UserAnswers.objects.get(possible_answer=possible_answer, player=player)
