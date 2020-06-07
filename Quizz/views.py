46ddbbeac95248c691216d74d92ac1c3c14a225a
Thibaut Meunier <wiirio.meunier@gmail.com>
2020-06-07 15:55:31 +0200

    Revert "Safety for empty category list in post request"
    This reverts commit 58013bcedb5593dd36d1019df218caeddd906586.

--- Quizz/views.py
+++ Quizz/views.py
@@ -240,7 +240,8 @@ def creation(request):
         title = request.POST.get('form_title')
         description = request.POST.get('form_description')
         author = getUserByLogin(request.session['login'])
-        categories = request.POST.get('category_list').split(';').remove('')
+        categories = request.POST.get('category_list').split(';')
+        if '' in categories: categories.remove('')
         form = addQuizzForm(title, author, description, categories)
 
         nbQuestions = request.POST.get('nbQuestions')
